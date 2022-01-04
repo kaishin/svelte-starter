e108b31282d128e79743b3d2abbad9a27224d6e0
Reda Lemeden <git@redalemeden.com>
2022-01-04 16:09:36 +0100

    Switch to adapter-static

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main>
+  <div class="container">
+    <h1>Homepage</h1>
+    <hr>
+    <p>This is my new SvelteKit app.</p>
+  </div>
+</main>
+
+<script context="module">
+  export const hydrate = false;
+  export const router = false;
+</script>
+
+<style>
+  main {
+    font-family: sans-serif;
+  }
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0px 20px;
+  }
+
+  .container > * {
+    width: 100%;
+    max-width: 700px;
+  }
+</style>
